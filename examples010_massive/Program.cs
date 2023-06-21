e28e0f0cd9b8c0bc8adfb44e226cd55c6b290da5
Bella <bella.bedoshvili@gmail.com>
2023-06-22 00:53:46 +0300

    added new projects 008-011

---
+++ examples010_massive/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = { 12, 3, 4, 85, 69, 39, 85, 69, 39 };
+
+int n = array.Length;
+
+int find = 69;
+
+int index = 0;
+while(index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
